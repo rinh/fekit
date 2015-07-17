500d8c06f7f3cc573a223ef600ab927ace35d79e
Ice White <yashiro1899@gmail.com>
2015-07-17 10:42:17 +0800

    1437100937

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,17 +1,9 @@
-var npm = require('npm');
 var path = require('path');
+var spawn = require('child_process').spawn;
 
 var sassPath = path.resolve(__dirname, '../vendors/node-sass/');
 process.chdir(sassPath);
 
-npm.load({}, function(err, npm) {
-    if (err) {
-        console.error(err);
-        return null;
-    }
-
-    console.log("prefix = %s", npm.prefix);
-    npm.commands.install([], function(err) {
-        if (err) console.error(err);
-    });
+spawn('npm', ["install", "--production"], {
+    stdio: 'inherit'
 });
